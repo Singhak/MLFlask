46f072a6efcd7c264aabe135eabe3623fe81081c
singhak <anilsingh.singh09@gmail.com>
2022-03-20 12:09:50 +0530

    using sve model for prediction

--- app.py
+++ app.py
@@ -17,30 +17,32 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    df = pd.read_csv('spam.csv', encoding="latin-1")
-    df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
-    df['label'] = df['v1'].map({'ham': 0, 'spam': 1})
-    X = df['v2']
-    y = df['label']
-    cv = CountVectorizer()
-    X = cv.fit_transform(X)  # Fit the Data
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.33, random_state=42)
-    # Naive Bayes Classifier
-    clf = MultinomialNB()
-    clf.fit(X_train, y_train)
-    clf.score(X_test, y_test)
+    # df = pd.read_csv('spam.csv', encoding="latin-1")
+    # df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
+    # df['label'] = df['v1'].map({'ham': 0, 'spam': 1})
+    # X = df['v2']
+    # y = df['label']
+    # cv = CountVectorizer()
+    # X = cv.fit_transform(X)  # Fit the Data
+    # X_train, X_test, y_train, y_test = train_test_split(
+    #     X, y, test_size=0.33, random_state=42)
+    # # Naive Bayes Classifier
+    # clf = MultinomialNB()
+    # clf.fit(X_train, y_train)
+    # clf.score(X_test, y_test)
+    # joblib.dump((cv, clf), 'NB_spam_model.pkl')
 
     if request.method == 'POST':
+        NB_spam_model = open('NB_spam_model.pkl', 'rb')
+        cv, clf = joblib.load(NB_spam_model)
         message = request.form['message']
         data = [message]
         vect = cv.transform(data).toarray()
         my_prediction = clf.predict(vect)
-        print(data)
+        # print(data)
         # a if condition else b
         if(message):
             output = 'Spam' if my_prediction[0] == 1 else 'Normal'
-            # return jsonify(my_prediction)
             return render_template('home.html', prediction_text='This is a {} message'.format(output))
         return render_template('home.html', prediction_text='Please enter valid text meaasage.')
 
